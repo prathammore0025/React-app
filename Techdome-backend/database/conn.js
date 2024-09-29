b3d4f0edb197a6581d1e8a94128b0f657ea2f398
root <root@ip-172-31-20-153.ap-south-1.compute.internal>
2024-09-29 18:26:15 +0000

    docker files update

--- Techdome-backend/database/conn.js
+++ Techdome-backend/database/conn.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DB).then(() => {
-    console.log("connection established...!");
-}).catch((error) => {
-    console.log(error);
+
+mongoose.connect("mongodb://root:12345678@mongodb:27017/mydatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+.then(async () => {
+    console.log("Connection established...!");
+})
+
+.catch((error) => {
+    console.error("Connection error:", error);
+});
+
